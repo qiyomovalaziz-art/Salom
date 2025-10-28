cce779a6946ef0d2b7f5c13b4daf3a6b8cc5a302
qiyomovalaziz-art <qiyomovalaziz@gmail.com>
2025-10-28 13:59:57 +0500

    Update obmen_bot_full.py

--- obmen_bot_full.py
+++ obmen_bot_full.py
@@ -5,7 +5,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 
 # ⚙️ Sozlamalar
 TOKEN = "8023020606:AAEmI5pl2JF7spmfSmqVQ8SRXzSqsbN8Rpk"
-GROUP_USERNAMES = ["@pubg_uzbchat1", "@sarmoyasiz_pulkopaytrish", "@yana_bir_guruh"]  # Guruhlar ro'yxati
+GROUP_USERNAMES = ["@pubg_uzbchat1", "@sarmoyasiz_pulkopaytrish", "@sarmoyasiz_pul_ishlash_yolari"]  # Guruhlar ro'yxati
 ADMIN_ID = 7973934849  # Faqat sizning Telegram ID'ingiz
 
 bot = Bot(token=TOKEN)
